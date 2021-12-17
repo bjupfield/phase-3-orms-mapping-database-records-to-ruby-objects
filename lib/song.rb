4dfbb3a37d6ff3a6b17a93db016297c0eeb64326
Bennett <bjupfield@gmail.com>
2021-12-17 14:27:02 -0600

    done

--- lib/song.rb
+++ lib/song.rb
@@ -49,4 +49,28 @@ class Song
     song.save
   end
 
+  def self.new_from_db(row)
+    self.new(id: row[0], name: row[1], album: row[2])
+  end
+
+  def self.all
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+      SQL
+    DB[:conn].execute(sql).map do |object|
+      self.new_from_db(object)
+    end
+  end
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+    DB[:conn].execute(sql, name).map do |notalwaysfirst|
+      self.new_from_db(notalwaysfirst)
+    end.first
+  end
 end
